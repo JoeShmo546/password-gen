4b11181635116325a4fc45747549f1b417fcef64
omicron <josephblaaa@gmail.com>
2023-10-25 20:56:46 -0500

    repeat (sort of), still segmentation fault

--- main.c
+++ main.c
@@ -11,18 +11,40 @@ int main() {
 	
     srand(time(0));
     int passindex;	
+	int using = 0;
 	
-	for (int i = 0; i < strlen(password); i++){
-		passindex = rand() % (38 + 1 - 0) + 1;
-		char passchar = passarray[passindex];
-		strncat(password, &passchar, 1);
+	while(using == 0){
+
+		for (int i = 0; i < strlen(password); i++){
+			passindex = rand() % (38 + 1 - 0) + 1;
+			char passchar = passarray[passindex];
+			strncat(password, &passchar, 1);
+		}
+
+		password[0] = '\b';
+		
+		while(strlen(password) > 20){
+			password[strlen(password)-1] = '\0';
+		}
+		
+		printf("password: %s \n",password);
+
+		//password[0]= '\0';
+		
+		fflush(stdin);
+		char input = getchar();
+
+		if(input == 'p'){
+			continue;
+		}
+		else if (input == 'q'){
+			return 0;
+		}
+		else{
+			fflush(stdin);
+			printf("that wasn't one of the options");
+		}
 	}
-	
-	while(strlen(password) > 20){
-		password[strlen(password)-1] = '\0';
-	}
-	
-	printf("this is fine: %s \n",password);
 
 	return 0;
 }
